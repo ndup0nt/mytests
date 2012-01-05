f0d1af92ecd33c6f9fb13ac9b169f8cbc2a9584f
Nicolas Dupont <nicolas.dupont@oosphere.com>
2012-01-05 17:46:37 +0100

    formation étape 5

--- mytests-war/src/main/java/com/silverpeas/components/mytests/servlets/MyTestsRequestRouter.java
+++ mytests-war/src/main/java/com/silverpeas/components/mytests/servlets/MyTestsRequestRouter.java
@@ -23,85 +23,88 @@
  */
 package com.silverpeas.components.mytests.servlets;
 
-import javax.servlet.http.HttpServletRequest;
-import com.stratelia.silverpeas.silvertrace.SilverTrace;
-import com.stratelia.silverpeas.peasCore.servlets.ComponentRequestRouter;
-import com.stratelia.silverpeas.peasCore.MainSessionController;
+import com.silverpeas.components.mytests.control.MyTestsSessionController;
 import com.stratelia.silverpeas.peasCore.ComponentContext;
 import com.stratelia.silverpeas.peasCore.ComponentSessionController;
+import com.stratelia.silverpeas.peasCore.MainSessionController;
+import com.stratelia.silverpeas.peasCore.servlets.ComponentRequestRouter;
+import com.stratelia.silverpeas.silvertrace.SilverTrace;
 import com.stratelia.webactiv.beans.admin.UserDetail;
 
-import com.silverpeas.components.mytests.control.MyTestsSessionController;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyTestsRequestRouter extends ComponentRequestRouter {
+    private final Map<String, String> viewMappings = new HashMap<String, String>();
 
-	/**
-	 * This method has to be implemented in the component request rooter class.
-	 * returns the session control bean name to be put in the request object ex
-	 * : for almanach, returns "almanach"
-	 */
-	@Override
-	public String getSessionControlBeanName() {
-		return "MyTests";
-	}
+    public MyTestsRequestRouter() {
+        super();
+        viewMappings.put("Main", "welcome.jsp");
+        viewMappings.put("MyContacts", "mycontacts.jsp");
+    }
 
-	/**
-	 * Method declaration
-	 * 
-	 * 
-	 * @param mainSessionCtrl
-	 * @param componentContext
-	 * 
-	 * @return
-	 * 
-	 * @see
-	 */
-	@Override
-	public ComponentSessionController createComponentSessionController(
-			MainSessionController mainSessionCtrl,
-			ComponentContext componentContext) {
-		return new MyTestsSessionController(mainSessionCtrl, componentContext);
-	}
+    /**
+     * This method has to be implemented in the component request rooter class.
+     * returns the session control bean name to be put in the request object ex
+     * : for almanach, returns "almanach"
+     */
+    @Override
+    public String getSessionControlBeanName() {
+        return "MyTests";
+    }
 
-	/**
-	 * This method has to be implemented by the component request rooter it has
-	 * to compute a destination page
-	 * 
-	 * @param function
-	 *            The entering request function (ex : "Main.jsp")
-	 * @param componentSC
-	 *            The component Session Control, build and initialised.
-	 * @return The complete destination URL for a forward (ex :
-	 *         "/almanach/jsp/almanach.jsp?flag=user")
-	 */
-	@Override
-	public String getDestination(String function,
-			ComponentSessionController componentSC, HttpServletRequest request) {
-		String destination = "";
-		MyTestsSessionController mytestsSC = (MyTestsSessionController) componentSC;
-		SilverTrace.info("mytests", "MyTestsRequestRouter.getDestination()",
-				"root.MSG_GEN_PARAM_VALUE", "User=" + componentSC.getUserId()
-						+ " Function=" + function);
-		try {
-			if (function.startsWith("Main")) {
-				destination = "welcome.jsp";
-			} else {
-				destination = "welcome.jsp";
-			}
-			destination = "/mytests/jsp/" + destination;
-		} catch (Exception e) {
-			request.setAttribute("javax.servlet.jsp.jspException", e);
-			destination = "/admin/jsp/errorpageMain.jsp";
-		}
+    /**
+     * Method declaration
+     *
+     * @param mainSessionCtrl
+     * @param componentContext
+     * @return
+     * @see
+     */
+    @Override
+    public ComponentSessionController createComponentSessionController(
+            MainSessionController mainSessionCtrl,
+            ComponentContext componentContext) {
+        return new MyTestsSessionController(mainSessionCtrl, componentContext);
+    }
 
-		UserDetail userDetail = componentSC.getUserDetail();
-		String[] userRoles = componentSC.getUserRoles();
-		request.setAttribute("userDetail", userDetail);
-		request.setAttribute("userRoles", userRoles);
-		
-		SilverTrace.info("mytests", "MyTestsRequestRouter.getDestination()",
-				"root.MSG_GEN_PARAM_VALUE", "Destination=" + destination);
-		return destination;
-	}
+    /**
+     * This method has to be implemented by the component request rooter it has
+     * to compute a destination page
+     *
+     * @param function    The entering request function (ex : "Main.jsp")
+     * @param componentSC The component Session Control, build and initialised.
+     * @return The complete destination URL for a forward (ex :
+     *         "/almanach/jsp/almanach.jsp?flag=user")
+     */
+    @Override
+    public String getDestination(String function,
+                                 ComponentSessionController componentSC, HttpServletRequest request) {
+        String destination;
+        SilverTrace.info("mytests", "MyTestsRequestRouter.getDestination()",
+                "root.MSG_GEN_PARAM_VALUE", "User=" + componentSC.getUserId()
+                + " Function=" + function);
+        try {
+            if (viewMappings.containsKey(function)) {
+                destination = "/mytests/jsp/" + viewMappings.get(function);
+            } else {
+                destination = "/admin/jsp/errorpageMain.jsp";
+            }
+        } catch (Exception e) {
+            request.setAttribute("javax.servlet.jsp.jspException", e);
+            destination = "/admin/jsp/errorpageMain.jsp";
+        }
+
+        UserDetail userDetail = componentSC.getUserDetail();
+        String[] userRoles = componentSC.getUserRoles();
+        request.setAttribute("userDetail", userDetail);
+        request.setAttribute("userRoles", userRoles);
+        request.setAttribute("action", function);
+
+        SilverTrace.info("mytests", "MyTestsRequestRouter.getDestination()",
+                "root.MSG_GEN_PARAM_VALUE", "Destination=" + destination);
+        return destination;
+    }
 
 }
--- mytests-war/src/main/webapp/mytests/jsp/welcome.jsp
+++ mytests-war/src/main/webapp/mytests/jsp/welcome.jsp
@@ -23,34 +23,42 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 --%>
-<%@ include file="check.jsp"%>
+<%@ include file="include/check.jsp" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
-<%@ taglib uri="http://www.silverpeas.com/tld/viewGenerator" prefix="view"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ taglib uri="http://www.silverpeas.com/tld/viewGenerator"
+           prefix="view" %>
 <html>
-  <head>
-    <view:looknfeel />
-  </head>
-  <c:set var="componentId" value="${requestScope.componentId}" />
-  <c:set var="sessionController">Silverpeas_MyTests_<c:out value="${componentId}" /></c:set>
-  <fmt:setLocale value="${sessionScope[sessionController].language}" />
-  <view:setBundle bundle="${requestScope.resources.multilangBundle}" />
-  <view:setBundle bundle="${requestScope.resources.iconsBundle}" var="icons" />
-  <c:set var="browseContext" value="${requestScope.browseContext}" />
-  <body bgcolor="#ffffff" leftmargin="5" topmargin="5" marginwidth="5" marginheight="5">
-    <view:window>
-      <view:frame>
-      	${requestScope.userDetail.firstName}, bienvenue sur le composant mytests.
-        <fmt:message key="mytests.welcome" />
+<head>
+    <view:looknfeel/>
+</head>
+<c:set var="componentId" value="${requestScope.componentId}"/>
+<view:setBundle bundle="${requestScope.resources.multilangBundle}"/>
+<view:setBundle bundle="${requestScope.resources.iconsBundle}"
+                var="icons"/>
+<c:set var="browseContext" value="${requestScope.browseContext}"/>
+<body bgcolor="#ffffff" leftmargin="5" topmargin="5" marginwidth="5"
+      marginheight="5">
+<c:set var="browseBarTitle">
+    <fmt:message key="mytests.browseBar.title"/>
+</c:set>
+<view:browseBar path="${browseBarTitle}"/>
+<view:window>
+    <%@ include file="include/tabs.jsp" %>
+    <view:frame>
+        ${requestScope.userDetail.firstName}, <fmt:message
+            key="mytests.welcome"/>.
         Vous avez les droits suivants :
         <c:forEach var="role" items="${requestScope.userRoles}">${role},</c:forEach>
-      </view:frame>
-      <view:frame>
+    </view:frame>
+    <view:frame>
         <view:board>
-          Cette instance s'appele <b><c:out value="${browseContext[1]}" /></b>.<br/>
-          Elle se situe dans l'espace <b><c:out value="${browseContext[0]}" /></b>.
+            Cette instance s'appele <b><c:out
+                value="${browseContext[1]}"/></b>.<br />
+            Elle se situe dans l'espace <b><c:out
+                value="${browseContext[0]}"/></b>.
         </view:board>
-      </view:frame>
-    </view:window>
-  </body>
+    </view:frame>
+</view:window>
+</body>
 </html>
